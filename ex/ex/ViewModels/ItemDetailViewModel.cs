fa34ec7d008bd544face645975775369e9730252
Fernando Inzunza <ferinzo96@gmail.com>
2020-03-02 13:13:04 -0700

    Agregar archivos de proyecto.

---
+++ ex/ex/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ex.Models;
+
+namespace ex.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
